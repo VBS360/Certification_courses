67ad629e239fe5c2d24d02c994786de0c8e84bdb
Vatsal Shah <96993029+VBS360@users.noreply.github.com>
2024-09-17 00:54:16 +0530

    Delete Day 17/quiz_game_start directory

--- Day 17/quiz_game_start/main.py
+++
@@ -1,7 +0,0 @@
-class Question:
-    def __init__(self,text,answer):
-        self.text = text
-        self.answer = answer
-
-new_question = Question("ABCD","Self")
-print(new_question.text, new_question.answer)
