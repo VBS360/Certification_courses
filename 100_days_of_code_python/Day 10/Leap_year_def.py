b5815a92a8941515c8e45be7dd0c5d4b6cc06a6f
Vatsal Shah <96993029+VBS360@users.noreply.github.com>
2024-12-25 12:41:59 +0530

    Update Leap_year_def.py

--- 100_days_of_code_python/Day 10/Leap_year_def.py
+++ 100_days_of_code_python/Day 10/Leap_year_def.py
@@ -1,9 +1,15 @@
+year = int(input("Enter the year.: "))
+
 def is_leap_year(year):
-    if year % 4 == 0 and year % 400 == 0:
-        return True
-    elif year % 100 == 0 or year % 4 != 0:
-        return False
-    elif year % 4 == 0:
+    is_leap = True
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return  True
+            return False
         return True
 
-print(is_leap_year(year))
+if is_leap_year(year):
+    print(f"The year {year} is a leap year.")
+else:
+    print(f"The year {year} is not a leap year.")
