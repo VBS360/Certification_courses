b0725acbb434d78523d407022217bf9641234658
Vatsal Shah <96993029+VBS360@users.noreply.github.com>
2024-12-26 11:18:34 +0530

    Update 1. Prime Checker.py

--- 100_days_of_code_python/Day 12/1. Prime Checker.py
+++ 100_days_of_code_python/Day 12/1. Prime Checker.py
@@ -1,11 +1,12 @@
-def is_prime(number):
-    if number > 1:
-        for digit in range (2,int(number**0.5) + 1):
-            if number % digit == 0:
+num = int(input("Enter the number you want to check.: "))
+
+def prime_checker():
+    if num > 1:
+        for digit in range (2, int(num**0.5)+1):
+            if num % digit == 0:
                 return False
-        return True
+            return True
     else:
         return False
 
-is_prime (75)
-print(is_prime(75))
+print(prime_checker())
